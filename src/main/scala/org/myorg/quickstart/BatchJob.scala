0ee0854b8bd003a18dffe5272870bfd171561a34
彼岸529 <zhangpei529@vip.qq.com>
2020-07-17 19:57:49 +0800

    flink批处理跑通

--- src/main/scala/org/myorg/quickstart/BatchJob.scala
+++ src/main/scala/org/myorg/quickstart/BatchJob.scala
@@ -33,34 +33,13 @@ import org.apache.flink.api.scala._
 object BatchJob {
 
   def main(args: Array[String]) {
-    // set up the batch execution environment
     val env = ExecutionEnvironment.getExecutionEnvironment
+    val text = env.readTextFile("data/wc.txt")
 
-    /*
-     * Here, you can start creating your execution plan for Flink.
-     *
-     * Start with getting some data from the environment, like
-     *  env.readTextFile(textPath);
-     *
-     * then, transform the resulting DataSet[String] using operations
-     * like
-     *   .filter()
-     *   .flatMap()
-     *   .join()
-     *   .group()
-     *
-     * and many more.
-     * Have a look at the programming guide:
-     *
-     * https://flink.apache.org/docs/latest/apis/batch/index.html
-     *
-     * and the examples
-     *
-     * https://flink.apache.org/docs/latest/apis/batch/examples.html
-     *
-     */
-
-    // execute program
-    env.execute("Flink Batch Scala API Skeleton")
+    text.flatMap(_.split(","))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+      .print()
   }
 }
