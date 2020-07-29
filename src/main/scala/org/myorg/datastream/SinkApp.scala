95464ce91d18ceedfc44a7e109c0b92147f6d39c
彼岸529 <zhangpei529@vip.qq.com>
2020-07-29 20:24:49 +0800

    数据写入mysql

--- src/main/scala/org/myorg/datastream/SinkApp.scala
+++ src/main/scala/org/myorg/datastream/SinkApp.scala
@@ -1,9 +1,14 @@
 package org.myorg.datastream
 
+import java.sql.{Connection, PreparedStatement}
+
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 import org.apache.flink.streaming.connectors.redis.RedisSink
 import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
 import org.apache.flink.streaming.connectors.redis.common.mapper.{RedisCommand, RedisCommandDescription, RedisMapper}
+import org.myorg.utils.MySQLUtils
 
 object SinkApp {
   def main(args: Array[String]): Unit ={
@@ -14,18 +19,49 @@ object SinkApp {
       (splits(1).trim, splits(2).trim.toLong)
     }).keyBy(_._1).sum(1)
 
-    class MyRedisMapper extends RedisMapper[(String, Long)] {
-      override def getCommandDescription: RedisCommandDescription = {
-        new RedisCommandDescription(RedisCommand.HSET, "bigdata-traffics")
+//    class MyRedisMapper extends RedisMapper[(String, Long)] {
+//      override def getCommandDescription: RedisCommandDescription = {
+//        new RedisCommandDescription(RedisCommand.HSET, "bigdata-traffics")
+//      }
+//
+//      override def getKeyFromData(t: (String, Long)): String = t._1
+//
+//      override def getValueFromData(t: (String, Long)): String = t._2 + ""
+//
+//
+//    }
+//    val conf = new FlinkJedisPoolConfig.Builder().setHost("192.168.1.141").build()
+//    value.addSink(new RedisSink[(String, Long)](conf, new MyRedisMapper))
+
+    class MySQLSink extends RichSinkFunction[(String, Long)]{
+      var connection: Connection = _
+      var insertPsmt: PreparedStatement = _
+      var updatePsmt: PreparedStatement = _
+      override def open(parameters: Configuration): Unit = {
+        connection = MySQLUtils.getConnection()
+        insertPsmt = connection.prepareStatement("insert into traffic(domain , traffics) values(?,?) ")
+        updatePsmt = connection.prepareStatement("update traffic set traffics = ? where domain = ? ")
       }
 
-      override def getKeyFromData(t: (String, Long)): String = t._1
+      override def close(): Unit = {
+        insertPsmt.close()
+        updatePsmt.close()
+        connection.close()
+      }
 
-      override def getValueFromData(t: (String, Long)): String = t._2 + ""
+      override def invoke(value: (String, Long)): Unit = {
+        updatePsmt.setLong(1, value._2)
+        updatePsmt.setString(2 , value._1)
+        updatePsmt.execute()
 
-      val conf = new FlinkJedisPoolConfig.Builder().setHost("192.168.1.141").build()
-      value.addSink(new RedisSink[(String, Long)](conf, new MyRedisMapper))
-      env.execute()
+        if (updatePsmt.getUpdateCount == 0){
+          insertPsmt.setString(1, value._1)
+          insertPsmt.setLong(2, value._2)
+          insertPsmt.execute()
+        }
+      }
     }
+    value.addSink(new MySQLSink)
+    env.execute()
   }
 }
